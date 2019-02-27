267576f2ebd9f6764e8a70a33cd354733872266d
Philip Poppler <philpoppler@gmail.com>
2019-02-27 12:50:22 -0800

    Add RGB Values and More Options

--- color-classifier-react/src/ColorInterface.jsx
+++ color-classifier-react/src/ColorInterface.jsx
@@ -13,7 +13,6 @@ export default class ColorInterface extends Component {
       colors: initializeColor()
     };
     this.changeColor = this.changeColor.bind(this);
-    
   }
 
   changeColor(type) {
@@ -43,63 +42,80 @@ export default class ColorInterface extends Component {
 
   render() {
     return (
-      <div className="container">
-        <Row>
-          <Col xs={4}>
-            <div
-              className="color-box"
-              style={{ backgroundColor: `rgb(${this.state.colors})` }}
-            />
-          </Col>
-          <Col xs={8}>
-            <h1>Color Crowdsource Recognition</h1>
-            <p>Using this tool, determine what type of color it is</p>
-            <ButtonGroup>
-              <Button
-                onClick={() => this.changeColor("Red-ish")}
-                style={{ backgroundColor: "firebrick" }}
-              >
-                Red-ish
-              </Button>
-              <Button
-                onClick={()=> this.changeColor("Green-ish")}
-                style={{ backgroundColor: "limegreen" }}
-              >
-                Green-ish
-              </Button>
-              <Button
-                onClick={()=> this.changeColor("Blue-ish")}
-                style={{ backgroundColor: "royalblue" }}
-              >
-                Blue-ish
-              </Button>
-              <Button
-                onClick={()=> this.changeColor("Brown-ish")}
-                style={{ backgroundColor: "saddlebrown" }}
-              >
-                Brown-ish
-              </Button>
-              <Button
-                onClick={()=> this.changeColor("Orange-ish")}
-                style={{ backgroundColor: "orange" }}
-              >
-                Orange-ish
-              </Button>
-              <Button
-                onClick={()=> this.changeColor("Pink-ish")}
-                style={{ backgroundColor: "pink" }}
-              >
-                Pink-ish
-              </Button>
-              <Button
-                onClick={()=> this.changeColor("Grey-ish")}
-                style={{ backgroundColor: "grey" }}
-              >
-                Grey-ish
-              </Button>
-            </ButtonGroup>
-          </Col>
-        </Row>
+      <div
+        className="App-header"
+        style={{
+          background: `linear-gradient(rgb(${this.state.colors}), #353535)`
+        }}
+      >
+        <div className="container">
+          <Row>
+            <Col xs={4}>
+              <div
+                className="color-box"
+                style={{ backgroundColor: `rgb(${this.state.colors})` }}
+              />
+              <p>R: {this.state.colors[0]} G: {this.state.colors[1]} B: {this.state.colors[2]}</p>
+            </Col>
+            <Col xs={8}>
+              <h1>Color Crowdsource Recognition</h1>
+              <p>Using this tool, determine what type of color it is</p>
+              <ButtonGroup>
+                <Button
+                  onClick={() => this.changeColor("Red-ish")}
+                  style={{ backgroundColor: "firebrick" }}
+                >
+                  Red-ish
+                </Button>
+                <Button
+                  onClick={() => this.changeColor("Green-ish")}
+                  style={{ backgroundColor: "limegreen" }}
+                >
+                  Green-ish
+                </Button>
+                <Button
+                  onClick={() => this.changeColor("Blue-ish")}
+                  style={{ backgroundColor: "royalblue" }}
+                >
+                  Blue-ish
+                </Button>
+                <Button onClick={() => this.changeColor("Yellow-ish")}
+                style={{backgroundColor: "goldenrod"}}
+                >
+                  Yellow-ish
+                </Button>
+                <Button
+                  onClick={() => this.changeColor("Brown-ish")}
+                  style={{ backgroundColor: "saddlebrown" }}
+                >
+                  Brown-ish
+                </Button>
+                <Button
+                  onClick={() => this.changeColor("Orange-ish")}
+                  style={{ backgroundColor: "orange" }}
+                >
+                  Orange-ish
+                </Button>
+                <Button 
+                  onClick={() => this.changeColor("Purple-ish")}
+                  style={{backgroundColor: "blueviolet"}}
+                >Purple-ish</Button>
+                <Button
+                  onClick={() => this.changeColor("Pink-ish")}
+                  style={{ backgroundColor: "violet" }}
+                >
+                  Pink-ish
+                </Button>
+                <Button
+                  onClick={() => this.changeColor("Grey-ish")}
+                  style={{ backgroundColor: "grey" }}
+                >
+                  Grey-ish
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
